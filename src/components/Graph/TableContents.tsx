e8d310d5dc27dec428ddd758a5042bc4e3736c3c
Laura Migueláñez <laura.miguelanez@gmail.com>
2023-08-12 21:41:15 +0200

    open blank

--- src/components/Graph/TableContents.tsx
+++ src/components/Graph/TableContents.tsx
@@ -69,7 +69,6 @@ const NavWrapper = styled.div<{ height: number }>`
 const NavEl = styled.div`
   margin-bottom: 20px;
   overflow: hidden;
-  color: blue;
 `;
 
 const NavText = styled.h3<{ hover: boolean }>`
@@ -147,7 +146,7 @@ const NavElement: React.FC<{
   return (
     <NavEl
       onClick={() => {
-        if (path) window.open(path, '_self', 'noreferrer'); //window.location.href = path;
+        if (path) window.open(path, '_blank', 'noreferrer'); //window.location.href = path;
       }}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
